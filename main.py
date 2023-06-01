f5f3ddd23ba5ebbaee67ce97e5c726c0b7f5d1a0
EvgenyV <jekajuk@gmail.com>
2023-06-01 18:06:50 +0300

    Arguments reader from cmd

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ import Tools
 from Data import Data
 
 data = Data()
-file_name_1 = 'AA raw data 11.csv'
-file_name_2 = 'sample raw data.csv'
 
-N_MIN = 15
-N_MAX = 60
-
-y_min = 2000
-y_max = 2020
-
-Tools.read_csv_to_list(file_name_1, data)
+args_list = Tools.cmd_args_reader()
+Tools.read_csv_to_list(args_list[0], data)
 Tools.adding_missing_information(data)
 Tools.clean_unnecessary_data(data)
-Tools.sort_by_year_to_dictionary(y_min, y_max, data)
-Tools.prices_comparison_average_ss_calculation(N_MIN, N_MAX, y_min, y_max, data)
+Tools.sort_by_year_to_dictionary(args_list[3], args_list[4], data)
+Tools.prices_comparison_average_ss_calculation(args_list[1], args_list[2], args_list[3], args_list[4],
+                                               data, args_list[5], args_list[6])
 print(*data.result_data, sep="\n")
